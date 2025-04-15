948519db6ae9c9e9f7c1f687630eacf053ee604e
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-04-15 14:07:02 +0900

    [add] Scriptsファイルのお引越し

---
+++ Assets/Scripts/InGame/Exhibit/FlightController.cs
@@ -0,0 +1,101 @@
+using System;
+using DebugPlayer;
+using UnityEngine;
+
+namespace September.InGame
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class FlightController : MonoBehaviour
+    {
+        [SerializeField] private Move _move;
+        [SerializeField] private float _moveSpeed = 10f;
+        [SerializeField] private float _rotationSpeed = 100f;
+        [SerializeField] private float _verticalSpeed = 5f;
+        [SerializeField] private float _flightDuration = 5f;
+        [SerializeField] private float _timer;
+
+        public bool _isFlying;
+        public bool _isStart;
+
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _isFlying = false;
+        }
+
+        private void Update()
+        {
+            if (_isStart && Input.GetKeyDown(KeyCode.Space))
+            {
+                _isFlying = true;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_isFlying)
+                return;
+            
+            _timer -= Time.fixedDeltaTime;
+            
+            if (_timer <= 0f)
+            {
+                StopFlight();
+                return;
+            }
+
+            // 入力取得
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            float upDownInput = 0f;
+
+            if (Input.GetKey(KeyCode.Space))
+                upDownInput += 1f;
+            if (Input.GetKey(KeyCode.LeftControl))
+                upDownInput -= 1f;
+
+            Rotate(horizontalInput);
+            Move(verticalInput, upDownInput);
+        }
+
+        private void Rotate(float input)
+        {
+            float yaw = input * _rotationSpeed * Time.fixedDeltaTime;
+            Quaternion rotation = Quaternion.Euler(0f, yaw, 0f);
+            _rigidbody.MoveRotation(_rigidbody.rotation * rotation);
+        }
+
+        private void Move(float forwardInput, float upDownInput)
+        {
+            Vector3 moveDirection = transform.forward * (forwardInput * _moveSpeed);
+            moveDirection += Vector3.up * (upDownInput * _verticalSpeed);
+            _rigidbody.linearVelocity = moveDirection;
+        }
+
+        public void StartFlight()
+        {
+            _isFlying = true;
+            _timer = _flightDuration;
+            _rigidbody.useGravity = false;
+        }
+
+        public void Set(bool a)
+        {
+            _isStart = a;
+        }
+
+        private void StopFlight()
+        {
+            _isFlying = false;
+            _rigidbody.useGravity = true;
+            _rigidbody.linearVelocity = Vector3.zero;
+        }
+    }
+}
