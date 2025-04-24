df233e7e7e0d97a1446dc72c374e1aa6cc3d9b69
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-04-24 22:30:39 +0900

    [Fix]通知の内容を変更

--- Assets/Scripts/InGame/UI/NoticeManager.cs
+++ Assets/Scripts/InGame/UI/NoticeManager.cs
@@ -8,11 +8,12 @@ public class NoticeManager : NetworkBehaviour
    public override void Spawned()
    {
       _text = GetComponent<Text>();
+      _text.text = "";
    }
 
-   public void UpdateNoticeText(string nickName)
+   public void UpdateNoticeText()
    {
-      _text.text = $"鬼がPLayer{nickName}に変更されました";
+      _text.text = "鬼が変更されました";
    }
 
    public void HideNoticeText()
