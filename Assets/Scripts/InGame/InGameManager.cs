a01d42b27db62eb1b87f9023b7440aee850e05c6
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-05-26 23:02:48 +0900

    [Add]Player気絶時の処理を追加（PlayerDataBaseに同じような記載あり）

--- Assets/Scripts/InGame/InGameManager.cs
+++ Assets/Scripts/InGame/InGameManager.cs
@@ -1,10 +1,14 @@
 using Fusion;
 using InGame.Player;
+using September.Common;
 using UnityEngine;
 
 public class InGameManager : NetworkBehaviour
 {
     [Networked] private TickTimer _tickTimer {get; set; }
+    
+    [Header("ゲーム時間（秒）")]
+    [SerializeField]
     private int _gameTime = 1200;
     
     public override void Spawned()
@@ -16,8 +20,16 @@ public class InGameManager : NetworkBehaviour
     /// <summary>
     /// 各Playerの気絶時に呼ばれるメソッド
     /// </summary>
-    public void RPC_OnPlayerKilled(PlayerManager killer, PlayerManager killed)
+    public void RPC_OnPlayerKilled(PlayerRef killer, PlayerRef killed)
     {
+        if (!Runner.IsServer) return;　 // サーバー側でのみ実行可能
+        var killerData = PlayerDatabase.Instance.PlayerDataDic.Get(killer);　　//DataBaseから該当Playerの情報取得
+        killerData.IsOgre = false;
+        PlayerDatabase.Instance.PlayerDataDic.Set(killer, killerData);　　　　　//DataBase更新 
+            
+        var killedData = PlayerDatabase.Instance.PlayerDataDic.Get(killed);
+        killedData.IsOgre = false;
+        PlayerDatabase.Instance.PlayerDataDic.Set(killed, killedData);
         //　IsOgreの切り替え、キルログを出すためのイベントInvoke、PlayerDataBase更新等
     }
 
