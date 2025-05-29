b234934397b8e0931e43870888f5cc0957b4252a
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2025-05-29 22:53:48 +0900

    [fix] GameLauncherの名前を変更。PlayerDatabaseのPlayerDataの名前を変更。

--- Assets/Scripts/Common/NetworkManager.cs
+++ Assets/Scripts/Common/NetworkManager.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Fusion;
-using Fusion.Sockets;
 using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace September.Common
 {
-    public class GameLauncher : MonoBehaviour, INetworkRunnerCallbacks
+    public class NetworkManager : MonoBehaviour
     {
-        public static GameLauncher Instance;
+        public static NetworkManager Instance;
         [SerializeField] NetworkRunner _runnerPrefab;
         [SerializeField] PlayerDatabase _playerDatabasePrefab;
         [SerializeField, Scene] string _titleSceneName;
@@ -24,7 +21,6 @@ namespace September.Common
             if (Instance == null)
             {
                 _networkRunner = Instantiate(_runnerPrefab);
-                _networkRunner.AddCallbacks(this);
                 Instance = this;
                 DontDestroyOnLoad(gameObject);
             }
@@ -77,14 +73,12 @@ namespace September.Common
         {
             await _networkRunner.Shutdown();
             _networkRunner = Instantiate(_runnerPrefab);
-            _networkRunner.AddCallbacks(this);
         }
         public async UniTaskVoid QuitLobby()
         {
             await _networkRunner.Shutdown();
             await SceneManager.LoadSceneAsync(_titleSceneName);
             _networkRunner = Instantiate(_runnerPrefab);
-            _networkRunner.AddCallbacks(this);
         }
 
         public async UniTaskVoid StartGame()
@@ -100,29 +94,5 @@ namespace September.Common
                     inputAuthority: pair.Key);
             }
         }
-        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
-        void INetworkRunnerCallbacks.OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
-        void INetworkRunnerCallbacks.OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
-        void INetworkRunnerCallbacks.OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
-        void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input) { }
-        void INetworkRunnerCallbacks.OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
-        void INetworkRunnerCallbacks.OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
-        void INetworkRunnerCallbacks.OnConnectedToServer(NetworkRunner runner) { }
-        void INetworkRunnerCallbacks.OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
-        void INetworkRunnerCallbacks.OnConnectRequest(NetworkRunner runner,
-            NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
-        void INetworkRunnerCallbacks.OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress,
-            NetConnectFailedReason reason) { }
-        void INetworkRunnerCallbacks.OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
-        void INetworkRunnerCallbacks.OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
-        void INetworkRunnerCallbacks.OnCustomAuthenticationResponse(NetworkRunner runner,
-            Dictionary<string, object> data) { }
-        void INetworkRunnerCallbacks.OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
-        void INetworkRunnerCallbacks.OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key,
-            ArraySegment<byte> data) { }
-        void INetworkRunnerCallbacks.OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key,
-            float progress) { }
-        void INetworkRunnerCallbacks.OnSceneLoadDone(NetworkRunner runner) { }
-        void INetworkRunnerCallbacks.OnSceneLoadStart(NetworkRunner runner) { }
     }
 }
