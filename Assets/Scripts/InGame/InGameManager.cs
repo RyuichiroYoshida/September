de5f0fe50ca0b28cb9fdded3a29560a1619a6538
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-05-30 21:33:30 +0900

    [Fix]NameSpace、書式修正等

--- Assets/Scripts/InGame/InGameManager.cs
+++ Assets/Scripts/InGame/InGameManager.cs
@@ -4,101 +4,106 @@ using System.Linq;
 using Fusion;
 using InGame.Player;
 using September.Common;
-using September.InGame.Common;
 using UnityEngine;
 
-public class InGameManager : NetworkBehaviour,IRegisterableService
+
+namespace September.InGame.Common
 {
-    [Networked] private TickTimer _tickTimer {get; set; }
-    
-    [Header("ゲーム時間（秒）")]
-    [SerializeField]
-    private int _gameTime = 1200;
-
-    public Action OnOgreChanged;
-    
-    private NetworkRunner _networkRunner;
-    
-    public async void InGameMInitialized()
+    public class InGameManager : NetworkBehaviour, IRegisterableService
     {
-        _networkRunner = FindFirstObjectByType<NetworkRunner>();
-        if (_networkRunner == null)
+        [Networked] private TickTimer _tickTimer { get; set; }
+
+        [Header("ゲーム時間（秒）")] [SerializeField] private int _gameTime = 1200;
+
+        public Action OnOgreChanged;
+
+        private NetworkRunner _networkRunner;
+
+        public async void InGameMInitialized()
         {
-            Debug.LogError("NetworkRunnerがありません");
+            _networkRunner = FindFirstObjectByType<NetworkRunner>();
+            if (_networkRunner == null)
+            {
+                Debug.LogError("NetworkRunnerがありません");
+            }
+
+            PlayerDatabase.Instance.ChooseOgre();
+            var container = CharacterDataContainer.Instance;
+            foreach (var pair in PlayerDatabase.Instance.PlayerDataDic)
+            {
+                var player = await _networkRunner.SpawnAsync(
+                    container.GetCharacterData(pair.Value.CharacterType).Prefab,
+                    inputAuthority: pair.Key);
+                var playerHealth = player.GetComponent<PlayerHealth>();
+                //PlayerHealthのOnDeathに登録
+            }
+
+            StartTimer();
+            HideCursor();
         }
-        PlayerDatabase.Instance.ChooseOgre();
-        var container = CharacterDataContainer.Instance;
-        foreach (var pair in PlayerDatabase.Instance.PlayerDataDic)
+
+
+        /// <summary>
+        /// 各Playerの気絶時に呼ばれるメソッド
+        /// </summary>
+        public void RPC_OnPlayerKilled(PlayerRef killer, PlayerRef killed)
         {
-            var player = await _networkRunner.SpawnAsync(container.GetCharacterData(pair.Value.CharacterType).Prefab,
-                inputAuthority: pair.Key);
-            var playerHealth = player.GetComponent<PlayerHealth>();
-            //PlayerHealthのOnDeathに登録
+            if (!Runner.IsServer) return; // サーバー側でのみ実行可能
+            var killerData = PlayerDatabase.Instance.PlayerDataDic.Get(killer); //DataBaseから該当Playerの情報取得
+            killerData.IsOgre = false;
+            PlayerDatabase.Instance.PlayerDataDic.Set(killer, killerData); //DataBase更新 
+
+            var killedData = PlayerDatabase.Instance.PlayerDataDic.Get(killed);
+            killedData.IsOgre = false;
+            PlayerDatabase.Instance.PlayerDataDic.Set(killed, killedData);
+            OnOgreChanged?.Invoke();
         }
-        StartTimer();
-        HideCursor();
-    }
-    
-    
-    
-    /// <summary>
-    /// 各Playerの気絶時に呼ばれるメソッド
-    /// </summary>
-    public void RPC_OnPlayerKilled(PlayerRef killer, PlayerRef killed)
-    {
-        if (!Runner.IsServer) return;　 // サーバー側でのみ実行可能
-        var killerData = PlayerDatabase.Instance.PlayerDataDic.Get(killer);　　//DataBaseから該当Playerの情報取得
-        killerData.IsOgre = false;
-        PlayerDatabase.Instance.PlayerDataDic.Set(killer, killerData);　　　　　//DataBase更新 
-            
-        var killedData = PlayerDatabase.Instance.PlayerDataDic.Get(killed);
-        killedData.IsOgre = false;
-        PlayerDatabase.Instance.PlayerDataDic.Set(killed, killedData);
-        OnOgreChanged?.Invoke();
-    }
 
-    void HideCursor()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    private void StartTimer()
-    {
-        _tickTimer = TickTimer.CreateFromSeconds(Runner,_gameTime);
-    }
-
-    private void GameEnded()
-    {
-        GetScore();
-    }
-
-    public IEnumerable<(PlayerRef,int score)> GetScore()
-    {
-        List<(PlayerRef player,int score)> _scores = new List<(PlayerRef,int)>();
-        foreach (var pair in PlayerDatabase.Instance.PlayerDataDic)
+        void HideCursor()
         {
-            _scores.Add((pair.Key,pair.Value.Score));
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
-        var ordered = _scores.OrderByDescending(x => x.score).ToList();
-        for (int i = 0; i < ordered.Count(); i++)
-        {
-            Debug.Log($"{i + 1} 位は{ordered[i].player}でスコアは{ordered[i].score}点");
-        }
-        return ordered;
-    }
 
-    public override void FixedUpdateNetwork()
-    {
-        if (_tickTimer.Expired(Runner))
+        private void StartTimer()
         {
-            GameEnded();
-            _tickTimer = TickTimer.None;
+            _tickTimer = TickTimer.CreateFromSeconds(Runner, _gameTime);
         }
-    }
-    
-    public void Register(ServiceLocator locator)
-    {
-        locator.Register<InGameManager>(this);
+
+        private void GameEnded()
+        {
+            GetScore();
+        }
+
+        public IEnumerable<(PlayerRef, int score)> GetScore()
+        {
+            List<(PlayerRef player, int score)> _scores = new List<(PlayerRef, int)>();
+            foreach (var pair in PlayerDatabase.Instance.PlayerDataDic)
+            {
+                _scores.Add((pair.Key, pair.Value.Score));
+            }
+
+            var ordered = _scores.OrderByDescending(x => x.score).ToList();
+            for (int i = 0; i < ordered.Count(); i++)
+            {
+                Debug.Log($"{i + 1} 位は{ordered[i].player}でスコアは{ordered[i].score}点");
+            }
+
+            return ordered;
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (_tickTimer.Expired(Runner))
+            {
+                GameEnded();
+                _tickTimer = TickTimer.None;
+            }
+        }
+
+        public void Register(ServiceLocator locator)
+        {
+            locator.Register<InGameManager>(this);
+        }
     }
 }
