7a50b9df396770f1406c35c14a6d0dff859593f8
TsuyoshiUsugi <qesulive@gmail.com>
2025-06-09 09:23:12 +0900

    [update] インタラクトにクールタイムとバリデート処理追加

--- Assets/Scripts/InGame/Interact/PlayerInteractionController.cs
+++ Assets/Scripts/InGame/Interact/PlayerInteractionController.cs
@@ -1,16 +1,12 @@
 using System;
+using System.Collections.Generic;
 using Fusion;
-using September.Common;
 using UnityEngine;
+using InGame.Interact;
+using September.Common;
 
 namespace InGame.Interact
 {
-    /// <summary>
-    /// インタラクト可能なオブジェクトのインターフェース
-    /// </summary>
-    /// <remarks>
-    /// このクラスは MonoBehaviour を継承しているため、Unity のコンポーネントとして使用できます。
-    /// </remarks>
     [RequireComponent(typeof(Collider))]
     [DisallowMultipleComponent]
     public class PlayerInteractionController : NetworkBehaviour
@@ -18,93 +14,133 @@ namespace InGame.Interact
         [SerializeField] private float _interactRadius = 2.5f;
         [SerializeField] private LayerMask _interactMask;
         [SerializeField] private Transform _interactOrigin;
+        [SerializeField] private float _baseInteractTime = 1.0f;
+        [SerializeField] private float _ogreInteractMultiplier = 1.0f;
+        [SerializeField] private CharacterType _characterType = CharacterType.OkabeWright;
 
         private readonly Collider[] _hitBuffer = new Collider[16];
-        private bool _wasInteractingLastFrame = false;
+
+        private bool _isInteracting = false;
+        private float _currentInteractTime = 0f;
+        private float _requiredInteractTime = 1.0f;
+
+        private InteractableBase _target;
+        private NetworkObject _targetNetObj;
+
+        public bool IsInteracting => _isInteracting;
+        public float CurrentInteractTime => _currentInteractTime;
 
         private void Awake()
         {
-            if (_interactOrigin == null)
+            if (!_interactOrigin)
             {
-                _interactOrigin = transform; // デフォルトでは自身の位置を使用
+                _interactOrigin = transform;
             }
         }
 
         public override void FixedUpdateNetwork()
         {
             if (!HasInputAuthority) return;
+            if (!GetInput(out PlayerInput input)) return;
 
-            if (GetInput(out PlayerInput input))
+            bool isHolding = input.Buttons.IsSet(PlayerButtons.Interact);
+
+            if (isHolding)
             {
-                bool isInteracting = input.Buttons.IsSet(PlayerButtons.Interact);
+                if (!_isInteracting)
+                    TryStartInteraction();
 
-                // トリガー型の操作にしたい場合：押した瞬間だけ反応
-                if (isInteracting && !_wasInteractingLastFrame)
+                if (_isInteracting)
                 {
-                    TryInteract();
+                    _currentInteractTime += Runner.DeltaTime;
+                    if (_currentInteractTime >= _requiredInteractTime)
+                    {
+                        CompleteInteraction();
+                    }
                 }
-
-                _wasInteractingLastFrame = isInteracting;
+            }
+            else
+            {
+                CancelInteraction();
             }
         }
 
-        private void TryInteract()
+        private void TryStartInteraction()
         {
-            int count = Physics.OverlapSphereNonAlloc(
-                _interactOrigin.position,
-                _interactRadius,
-                _hitBuffer,
-                _interactMask,
-                QueryTriggerInteraction.Collide
-            );
+            int count = Physics.OverlapSphereNonAlloc(_interactOrigin.position, _interactRadius, _hitBuffer, _interactMask);
 
             for (int i = 0; i < count; i++)
             {
                 var col = _hitBuffer[i];
-                if (col.TryGetComponent(out IInteractable interactable))
+                if (!col.TryGetComponent(out InteractableBase interactable)) continue;
+                if (!col.TryGetComponent(out NetworkObject netObj)) continue;
+
+                float baseTime = interactable.RequiredInteractTimeDictionary.Dictionary
+                    .GetValueOrDefault(_characterType, 0f);
+
+                float multiplier = 1f;
+
+                if (PlayerDatabase.Instance.PlayerDataDic.TryGet(Object.InputAuthority, out var playerData) && playerData.IsOgre)
                 {
-                    var context = new InteractableContext
-                    {
-                        Interactor = Object.InputAuthority.RawEncoded,
-                        WorldPosition = _interactOrigin.position
-                    };
-
-                    // Hostであれば直接呼ぶ
-                    if (HasStateAuthority)
-                    {
-                        interactable.Interact(context);
-                    }
-                    else
-                    {
-                        RPC_RequestInteract(Object.InputAuthority, col.GetComponent<NetworkObject>());
-                    }
-
-                    break; // 最初の対象だけ
+                    multiplier = _ogreInteractMultiplier;
                 }
+
+                _requiredInteractTime = baseTime * multiplier;
+                _currentInteractTime = 0f;
+
+                _target = interactable;
+                _targetNetObj = netObj;
+                _isInteracting = true;
+
+                return;
             }
+        }
+
+        private void CompleteInteraction()
+        {
+            _isInteracting = false;
+
+            var context = new InteractableContext
+            {
+                Interactor = Object.InputAuthority.RawEncoded,
+                WorldPosition = _interactOrigin.position,
+                RequiredInteractTime = _requiredInteractTime
+            };
+
+            if (HasStateAuthority)
+            {
+                _target?.Interact(context);
+            }
+            else
+            {
+                RPC_RequestInteract(context.Interactor, _targetNetObj, context.RequiredInteractTime);
+            }
+
+            _target = null;
+            _targetNetObj = null;
+        }
+
+        private void CancelInteraction()
+        {
+            _isInteracting = false;
+            _currentInteractTime = 0f;
+            _target = null;
+            _targetNetObj = null;
         }
 
         [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
-        private void RPC_RequestInteract(PlayerRef interactor, NetworkObject target)
+        private void RPC_RequestInteract(int interactor, NetworkObject target, float interactTime)
         {
-            if (target != null && target.TryGetComponent(out IInteractable interactable))
+            if (target != null && target.TryGetComponent(out InteractableBase interactable))
             {
                 var context = new InteractableContext
                 {
-                    Interactor = interactor.RawEncoded,
-                    WorldPosition = transform.position
+                    Interactor = interactor,
+                    WorldPosition = transform.position,
+                    RequiredInteractTime = interactTime
                 };
 
                 interactable.Interact(context);
-            }
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            if (_interactOrigin != null)
-            {
-                Gizmos.color = Color.cyan;
-                Gizmos.DrawWireSphere(_interactOrigin.position, _interactRadius);
             }
         }
     }
