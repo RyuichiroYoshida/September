3b64e7013919dc164a8815331705ea8b96d985d2
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-06-28 17:43:08 +0900

    [update] ゲームシーンの展示物の配置変更

--- Assets/Scripts/InGame/Exhibit/WarpObject.cs
+++ Assets/Scripts/InGame/Exhibit/WarpObject.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
     public class WarpObject : MonoBehaviour
     {
-        [SerializeField, Label("ワープエフェクト")] private ParticleSystem _warpEffect;
         [SerializeField, Label("ワープポジション")] private GameObject _warpPosition;
         [SerializeField, Label("音名")] private string _soundName;
         
         public Vector3 GetWarpPosition() => _warpPosition.transform.position;
-        public ParticleSystem GetWarpEffect() => _warpEffect;
         public string SoundName() => _soundName;
     }
