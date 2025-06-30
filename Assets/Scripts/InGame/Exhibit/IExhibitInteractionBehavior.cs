a28141ce17959e97a7c68a6d42f885f9e72a098e
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-06-30 16:20:32 +0900

    [add] プテラの展示物を更新

--- Assets/Scripts/InGame/Exhibit/IExhibitInteractionBehavior.cs
+++ Assets/Scripts/InGame/Exhibit/IExhibitInteractionBehavior.cs
@@ -2,6 +2,11 @@ namespace InGame.Exhibit
 {
     public interface IExhibitInteractionBehavior
     {
-        
+        // 爆弾設置
+        void OnSetBomb();       
+        // 破壊スキル
+        void OnDestroyAbility();
+        // 友好アクション
+        void OnFriend();        
     }
 }
